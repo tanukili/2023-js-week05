cd0f0a153f510a7b2a9ba5c50e0fe6235739421b
tanukile <backpacker320@gmail.com>
2023-11-08 01:23:33 +0800

    feat: 新增功能 & fix: 合併頁面

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,130 @@
 <script>
-import PackageCard from './components/PackageCard.vue';
-
 export default {
-  components: {
-    PackageCard,
+  data() {
+    return {
+      data: [
+        {
+          id: 0,
+          name: '肥宅心碎賞櫻3日',
+          imgUrl:
+            'https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80',
+          area: '高雄',
+          description: '賞櫻花最佳去處。肥宅不得不去的超讚景點！',
+          group: 87,
+          price: 1400,
+          rate: 10,
+        },
+        {
+          id: 1,
+          name: '貓空纜車雙程票',
+          imgUrl:
+            'https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+          area: '台北',
+          description:
+            '乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感',
+          group: 99,
+          price: 240,
+          rate: 2,
+        },
+        {
+          id: 2,
+          name: '台中谷關溫泉會1日',
+          imgUrl:
+            'https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80',
+          area: '台中',
+          description:
+            '全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。',
+          group: 20,
+          price: 1765,
+          rate: 7,
+        },
+      ],
+      newPackage: {
+        id: null,
+        name: '',
+        imgUrl: '',
+        area: '',
+        description: '',
+        group: null,
+        price: null,
+        rate: null,
+      },
+    };
+  },
+  methods: {
+    // 渲染方法
+    render() {
+      let cardsHtml = '';
+      this.data.forEach((e) => {
+        const template = `
+        <div class="col-4">
+          <div class="card shadow border-gray-300 h-100"">
+            <div class="position-relative">
+              <img
+                src="${e.imgUrl}"
+                class="card-img-top img-fluid"
+                alt="套票預覽${e.id}"
+                style="height: 180px"
+              />
+              <span
+                class="badge position-absolute translate-middle-y start-0 bg-info fs-5 py-2"
+                style="top: 5%"
+              >
+                ${e.area}
+              </span>
+              <span
+                class="badge position-absolute translate-middle-y start-0 top-100 bg-primary lh-base px-2"
+                style="width: 40px"
+              >
+                ${e.rate}
+              </span>
+            </div>
+            <div class="card-body pb-2">
+              <h3 class="card-title fs-4 border-bottom border-2 pb-1">
+                ${e.name}
+              </h3>
+              <p class="card-text lh-lg">
+                ${e.description}
+              </p>
+            </div>
+            <div
+              class="card-footer border-0 text-primary d-flex justify-content-between align-items-center"
+            >
+              <small class="d-flex align-items-center fs-6 fw-medium"
+                ><span
+                  class="material-symbols-outlined fs-5 me-1"
+                  style="font-variation-settings: 'FILL' 1"
+                >
+                  error </span
+                >剩下最後 ${e.group} 組</small
+              >
+              <p class="mb-0 fw-medium d-flex align-items-center">
+                TWD<span class="fs-2 ms-1">$${e.price}</span>
+              </p>
+            </div>
+          </div>
+        </div>`;
+        cardsHtml += template;
+      });
+      document.querySelector('.card-list').innerHTML = cardsHtml;
+    },
+  },
+  mounted() {
+    // 渲染畫面
+    this.render();
+    // 取 DON
+    const form = document.querySelector('form');
+    const submitBtn = document.querySelector('#submit');
+    // 監聽按鈕
+    submitBtn.addEventListener('click', () => {
+      window.event.preventDefault();
+      form.querySelectorAll('input, select, textarea').forEach((e) => {
+        this.newPackage[e.name] = e.value;
+      });
+      this.newPackage.id = this.data.length;
+      this.data.push({ ...this.newPackage });
+      this.render();
+    });
   },
 };
 </script>
@@ -39,6 +160,7 @@ export default {
                     <div class="col-9">
                       <input
                         type="text"
+                        name="name"
                         id="packageName"
                         class="form-control border-bottom rounded-top-1"
                         placeholder="請填寫套票名稱"
@@ -56,6 +178,7 @@ export default {
                     <div class="col-9">
                       <input
                         type="url"
+                        name="imgUrl"
                         id="packageUrl"
                         class="form-control border-bottom rounded-top-1"
                         placeholder="請填寫圖片網址"
@@ -73,12 +196,13 @@ export default {
                     <div class="col-9 select-icon position-relative">
                       <select
                         id="packArea"
+                        name="area"
                         class="form-select border-bottom rounded-top-1"
                       >
-                        <option>請選擇景點地區</option>
-                        <option value="taipei">台北</option>
-                        <option value="taichung">台中</option>
-                        <option value="kaohsiung">高雄</option>
+                        <option disabled selected hidden>請選擇景點地區</option>
+                        <option value="台北">台北</option>
+                        <option value="台中">台中</option>
+                        <option value="高雄">高雄</option>
                       </select>
                     </div>
                   </div>
@@ -92,8 +216,10 @@ export default {
                     </div>
                     <div class="col-9">
                       <input
-                        type="text"
+                        type="number"
+                        name="price"
                         id="packagePrice"
+                        min="1"
                         class="form-control border-bottom rounded-top-1"
                         placeholder="請填寫套票金額"
                       />
@@ -109,8 +235,10 @@ export default {
                     </div>
                     <div class="col-9">
                       <input
-                        type="text"
+                        type="number"
+                        name="group"
                         id="packageNum"
+                        min="1"
                         class="form-control border-bottom rounded-top-1"
                         placeholder="請填寫套票組數"
                       />
@@ -126,7 +254,10 @@ export default {
                     </div>
                     <div class="col-9">
                       <input
-                        type="text"
+                        type="number"
+                        name="rate"
+                        min="1"
+                        max="10"
                         id="packageStarts"
                         class="form-control border-bottom rounded-top-1"
                         placeholder="請填寫套票星級"
@@ -144,7 +275,7 @@ export default {
                     <div class="col-9">
                       <textarea
                         class="form-control border-bottom rounded-top-1"
-                        name="packageDescribe"
+                        name="description"
                         id="packageDescribe"
                         style="padding-bottom: 89px"
                         placeholder="請填寫套票描述（限 100 字）"
@@ -155,6 +286,7 @@ export default {
                     <div class="col-6 ms-auto">
                       <button
                         type="submit"
+                        id="submit"
                         class="btn btn-primary fs-5 mt-4 w-100"
                       >
                         新增套票
@@ -177,12 +309,13 @@ export default {
             <div class="col-9 select-icon position-relative">
               <select
                 class="form-select bg-white border border-gray-500 rounded-1"
+                id="areaFilter"
               >
-                <option>地區搜尋</option>
-                <option value="all">全部地區</option>
-                <option value="taipei">台北</option>
-                <option value="taichung">台中</option>
-                <option value="kaohsiung">高雄</option>
+                <option disabled hidden selected>地區搜尋</option>
+                <option value="全部地區">全部地區</option>
+                <option value="台北">台北</option>
+                <option value="台中">台中</option>
+                <option value="高雄">高雄</option>
               </select>
             </div>
           </div>
@@ -191,9 +324,7 @@ export default {
           <span class="text-secondary">本次搜尋共 6 筆資料</span>
         </div>
       </div>
-      <div class="row card-list">
-        <PackageCard />
-      </div>
+      <div class="row card-list gy-5"></div>
     </div>
   </div>
 </template>
@@ -230,5 +361,18 @@ export default {
 .select-icon::after {
   border-color: var(--bs-primary) transparent transparent transparent;
   bottom: 11px;
+}
+.gy-5 {
+  --bs-gutter-y: 38px;
+}
+.badge {
+  line-height: 1.2;
+  border-radius: 0 4px 4px 0;
+}
+option {
+  appearance: none;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  color: #212529;
 }
 </style>
